8c3cd9da01e6f554118f41031a617ebd035f1e76
Yang Guo <yangguo@chromium.org>
2017-09-15 12:38:42 +0200

    Add babel coverage demo.

---
+++ babel-coverage/demo.js
@@ -0,0 +1,5 @@
+module.paths.push(".");
+require("babel-register");
+debugger;
+require("babel-coverage/example.js");
+debugger;
---
+++ babel-coverage/example.js
@@ -0,0 +1,8 @@
+
+
+function f() {
+  console.log(arguments);
+}
+
+f(...[1,2,3]);
+debugger;
